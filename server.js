33d8880c8d63eaefffd59980128e4b5dae3ae4cf
Rishabh singh <rish29oo7@gmail.com>
2019-11-25 15:31:47 +0530

    initial commits

---
+++ server.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const app=express()
+// function uzzu(req,res){
+//     res.send('hello world')
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+app.listen(process.env.PORT,()=>(console.log("server started")))
